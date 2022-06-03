2645b16b2ec11b2d6b1b3de6412aefda9a337eca
Rafael Ahrons <rafael.ahrons@gmail.com>
2022-06-03 10:21:58 -0300

    First commit

---
+++ HunterAndPrey/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HunterAndPrey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
